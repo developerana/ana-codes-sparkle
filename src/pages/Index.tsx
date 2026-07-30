36d2baf472b451cec8a5c4c370421b4f20b3876d
Ana Helouise <101769601+anahelouise@users.noreply.github.com>
2026-07-29 22:44:13 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
-import { SkillsSection } from "DSDASD@/components/SkillsSection"; 
-import { ReelSection } from "ASDASDS@/components/ReelSection"; 
+import { SkillsSection } from "@/components/SkillsSection"; 
+import { ReelSection } from "@/components/ReelSection"; 
 import { ExperienceSection } from "@/components/ExperienceSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import { ContactSection } from "@/components/ContactSection";
